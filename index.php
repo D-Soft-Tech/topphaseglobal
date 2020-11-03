de27f61f4eb32d2e5475d1c7b7743c4ce45be9cc
D-Soft-Tech <dynamicsofttechnology@gmail.com>
2020-11-03 07:30:30 +0100

    final favicon

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <meta content="" name="keywords">
 
   <!-- Favicon -->
-  <link rel="shortcut icon" href="assets/img/newCorrectLogo.png">
+  <link rel="icon" type="image/ico" href="assets/img/newtopphasei.png">
   <!-- <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"> -->
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
